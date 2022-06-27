c3dbcc5ca512c83eb0e7f97a34dbaec856063b3f
Aleksandr <maivan007@yandex.ru>
2022-06-27 21:19:41 +0400

    Add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(max < numberB)
+{
+max = numberB;
+}
+if(max < numberC)
+{
+max = numberC;
+}
+Console.WriteLine("Из трех чисел " + numberA + ", " + numberB + ", " + numberC + " максимальным является число " + max);
