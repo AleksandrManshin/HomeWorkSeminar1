38afbfc836fd1615d87b62e0e28171e478885350
Aleksandr <maivan007@yandex.ru>
2022-06-27 21:11:07 +0400

    Add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+Console.WriteLine("Первое число " + numberA + " больше второго числа " + numberB);
+}
+if(numberA < numberB)
+{
+Console.WriteLine("Второе число " + numberB + " больше первого числа " + numberA);
+}
+if(numberA == numberB)
+{
+Console.WriteLine("Первое число " + numberA + " равно второму числу " + numberB);
+}
