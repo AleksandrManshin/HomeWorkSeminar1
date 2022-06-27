604fbb71ef5485839bbd25af8ee9f9b4e0c816c9
Aleksandr <maivan007@yandex.ru>
2022-06-27 21:25:49 +0400

    Add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число больше 1:");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while(count < number)
+{
+if(count % 2 == 0)
+{
+Console.Write(count + " ");
+}
+count++;
+}
+if(number <= 1)
+{
+Console.WriteLine("Число " + number + " меньше либо равно 1");
+}
